802d04d0a3d65396cf38289960b81bffb315165a
Kien Vu <kvstudio.154@gmail.com>
2022-04-15 21:48:32 +0700

    Update connectDB.php

--- assets/views/connectDB.php
+++ assets/views/connectDB.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
-	// $connect = new MySQLi("localhost", "son56648_spf", "Vu_Khac_Tiep@123", "son56648_spf");
-	$connect = new MySQLi("localhost", "root", "", "spf");
+	$connect = new MySQLi("localhost", "son56648_spf", "Vu_Khac_Tiep@123", "son56648_spf");
+	// $connect = new MySQLi("localhost", "root", "", "spf");
 	mysqli_set_charset($connect, 'UTF8');
 	if (mysqli_connect_errno()) {
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
